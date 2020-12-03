5cb651cac38ed6effb5f401e5ea0a579a123ea16
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-12-02 22:21:43 -0800

    dfs

--- template/dfs.cpp
+++ template/dfs.cpp
@@ -1,31 +1,44 @@
+/* template for dfs questions  */
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long int
+#define pb push_back
+#define debug(a) cout << #a << "=" << a << ' '<<endl;
+#define out(x)  for(auto &t:x) cout<<t<<" "; cout<<endl;
+#define all(x) x.begin(),x.end()
 
-const int mx=1e5+5;
+const ll mx=1e5+5;
 
-vector<int>adj[mx];
+const ll modu=1e9+7;
+
+vector<ll>adj[mx];
 bool vis[mx];
 
 
-void dfs(int v)
+void dfs(ll u)
 {
-	vis[v]=true;
-	for(int i=0;i<adj[v].size();++i)
+	vis[u]=true;
+	
+	for(auto child:adj[u])
 	{
-		if(!vis[adj[v][i]])
-		dfs(adj[v][i]);
+		if(!vis[child])
+		dfs(child);
+		
 	}
 }
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	
 	int t;
 	cin>>t;
 	while(t--)
 	{
-		int n,e;
-		cin>>n>>e;
+		ll n,m;
+		cin>>n>>m;
+		
 		/*  clearing the adjacency list */ 
 		for(int i=0;i<=mx;++i)
 		adj[i].clear();
@@ -34,17 +47,29 @@ int main()
 		/*   creating the graph          */
 		for(int i=0;i<e;++i)
 		{
-			int u,v;
+			ll u,v;
 			cin>>u>>v;
 			adj[u].push_back(v);
 			adj[v].push_back(u);
 		}
 		
-		
+		// dfs call
+		for(int i=1;i<=n;++i)
+		{
+			if(!vis[i])
+			{
+				
+				dfs(i);
+			}
+		}
 		
 		
 		
 	    
 	}
+	
+	
+	return 0;
+	
 	
 }
